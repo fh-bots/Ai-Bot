1f15b6762e9da8c2f2b0959c837e1fb4e3b3e979
fh_bots <69167693+fh-bots@users.noreply.github.com>
2021-02-07 17:13:13 +0530

    Update index.php

--- Bot/index.php
+++ Bot/index.php
@@ -70,13 +70,12 @@ I'm $BOT_NAME a powerfull AI Bot
 
 I will resopnd to any image you send. Send me a image to me, I will say what is That😜
 
-Also add me to your group and make me admin. I'll reply to every photo😌.***",
+That's all... ***",
 'reply_to_message_id'=>$message_id,
 'parse_mode'=>"MarkDown",
 'reply_markup' =>  json_encode([
 'inline_keyboard' => [
-[['text' => "SUPPORT",'url' => "https://telegram.me/induschats"],['text' => "CREATOR", 'url' => "https://telegram.me/benchamxd"]],
-[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Repo", 'url' => "https://github.com/Benchamxd/Ai-Bot"]], 
+[['text' => "FILIMHOUSE",'url' => "https://t.me/joinchat/Vsdqpx_0wNvfeiDx"]],
 ]])
 ]);
 }
